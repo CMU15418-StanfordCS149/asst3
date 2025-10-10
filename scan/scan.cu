630833d9082ce6c5661698cc034ed7665d264e6c
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2025-10-11 00:24:28 +0800

    在尝试提高这段代码并行化时，踩了不少坑，学到了很多有价值的东西，记录在【做 assignment3 part2 时一个有价值的探索、错误】

--- scan/scan.cu
+++ scan/scan.cu
@@ -27,32 +27,74 @@ static inline int nextPow2(int n) {
     return n;
 }
 
-// 排除前缀和的核函数
-__global__ void gpu_exclusive_scan(int* input, int N, int* result) {
-    // 计算线程对应的矩阵位置
+// 排除前缀和的核函数 (N 假设是2的幂)
+__global__ void gpu_exclusive_scan(int* input, int N, int* result, int* debug) {
+    // 计算 CUDA 线程ID
     int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+    printf("thread_id = %d\n", threadIdx.x);
 
-    // 这里只用一个线程来执行前缀和算法
-    if(thread_id != 0)
-        return;
-    
-    // 复制输入数组到输出数组
-    // memmove(result, input, N*sizeof(int));
-    for(int i = 0; i < N; i++) {
-        result[i] = input[i];
+    // cudaScan 函数已经让 result/debug 数组完全等同 input 数组，这里不需要再 memmove
+
+    // // upsweep阶段
+    // for (int two_d = 1; two_d <= N/2; two_d*=2) {
+    //     int two_dplus1 = 2*two_d;
+    //     // parallel_for (int i = 0; i < N; i += two_dplus1) {
+    //     for (int i = 0; i < N; i += two_dplus1) {
+    //         result[i+two_dplus1-1] += result[i+two_d-1];
+    //     }
+    // }
+    // thread 0，检测看 result 是否完全等于 debug
+    if(0 == thread_id) {
+        for(int i = 0; i < N; i++) {
+            if(result[i] != debug[i]) {
+                printf("Error at initialization: i = %d, debug = %d, result = %d\n", i, debug[i], result[i]);
+                return;
+            }
+        }
     }
+
+    // 同步所有线程
+    __syncthreads();
     
     // upsweep阶段
     for (int two_d = 1; two_d <= N/2; two_d*=2) {
         int two_dplus1 = 2*two_d;
-        // parallel_for (int i = 0; i < N; i += two_dplus1) {
-        for (int i = 0; i < N; i += two_dplus1) {
-            result[i+two_dplus1-1] += result[i+two_d-1];
+        for (int i = 0; i < N; i += two_dplus1 * THREADS_PER_BLOCK) {
+            // 防止越界
+            if(i + (thread_id) * two_dplus1 + two_dplus1 - 1 < N) {
+                if(i + (thread_id) * two_dplus1 + two_dplus1 - 1 == 131) {
+                    printf("thread %d, two_d %d: original result = %d, added result = %d\n", thread_id, two_d, result[i + (thread_id) * two_dplus1 + two_dplus1 - 1], result[i + (thread_id) * two_dplus1 + two_d - 1]);
+                }
+                result[i + (thread_id) * two_dplus1 + two_dplus1 - 1] += result[i + (thread_id) * two_dplus1 + two_d - 1];
+            }
+        }
+        // 同步所有线程
+        __syncthreads();
+        // 运行一遍 thread 0 单线程运算，检查看是否出错
+        if(0 == thread_id) {
+            for (int i = 0; i < N; i += two_dplus1) {
+                int tmp = debug[i+two_dplus1-1];
+                debug[i+two_dplus1-1] += debug[i+two_d-1];
+                if(debug[i+two_dplus1-1] != result[i+two_dplus1-1]) {
+                    printf("Error at upsweep: i = %d, two_d = %d, debug = %d, result = %d\n", i, two_d, debug[i+two_dplus1-1], result[i+two_dplus1-1]);
+                    printf("Error at upsweep: original debug[i+two_dplus1-1] = %d, debug[i+two_d-1] = %d\n", tmp, debug[i+two_d-1]);
+                    return;
+                }
+            }
         }
     }
 
-    result[N-1] = 0;
+    // 下面保持单线程，方便调试
+    if(thread_id > 0)
+        return;
 
+    // 这里仅需一个线程执行即可
+    if(thread_id == 0)
+        result[N-1] = 0;
+
+    // 同步所有线程
+    __syncthreads();
+    
     // downsweep阶段
     for (int two_d = N/2; two_d >= 1; two_d /= 2) {
         int two_dplus1 = 2*two_d;
@@ -62,6 +104,8 @@ __global__ void gpu_exclusive_scan(int* input, int N, int* result) {
             result[i+two_d-1] = result[i+two_dplus1-1];
             result[i+two_dplus1-1] += t;
         }
+        // // 同步所有线程
+        // __syncthreads();
     }
 }
 
@@ -80,7 +124,9 @@ __global__ void gpu_exclusive_scan(int* input, int N, int* result) {
 // Also, as per the comments in cudaScan(), you can implement an
 // "in-place" scan, since the timing harness makes a copy of input and
 // places it in result
-void exclusive_scan(int* input, int N, int* result)
+// void exclusive_scan(int* input, int N, int* result)
+// 为了方便调试，申请一块调试用内存
+void exclusive_scan(int* input, int N, int* result, int* debug)
 {
     // CS149 TODO:
     // Implement your exclusive scan implementation here.  Keep in
@@ -92,13 +138,12 @@ void exclusive_scan(int* input, int N, int* result)
     // 注意：这个函数是在CPU上运行的，但是传入的参数input/result是GPU上的数组
 
     // 定义线程块和网格大小
-    int blockSize = 256;  // 256个线程/块
-    int gridSize = (N + blockSize - 1) / blockSize; // 计算需要多少个块
-    // 计算下一个2次幂，方便简化算法的计算
+    int blockSize = THREADS_PER_BLOCK;  // 256个线程/块
     int rounded_length = nextPow2(N);
-
-    gpu_exclusive_scan<<<gridSize, blockSize>>>(input, rounded_length, result);
-
+    int gridSize = (rounded_length + blockSize - 1) / blockSize; // 计算需要多少个块，根据roundup后的长度
+    // 计算下一个2次幂，方便简化算法的计算 (这在最坏情况会引入2倍工作负载)
+    // Launch kernel over the rounded (power-of-two) length
+    gpu_exclusive_scan<<<gridSize, blockSize>>>(input, rounded_length, result, debug);
 }
 
 
@@ -128,6 +173,9 @@ double cudaScan(int* inarray, int* end, int* resultarray)
     
     cudaMalloc((void **)&device_result, sizeof(int) * rounded_length);
     cudaMalloc((void **)&device_input, sizeof(int) * rounded_length);
+    // 为了方便调试，申请一块调试用内存
+    int *device_debug;
+    cudaMalloc((void **)&device_debug, sizeof(int) * rounded_length);
 
     // For convenience, both the input and output vectors on the
     // device are initialized to the input values. This means that
@@ -136,10 +184,14 @@ double cudaScan(int* inarray, int* end, int* resultarray)
     // in mind when calling exclusive_scan from find_repeats.
     cudaMemcpy(device_input, inarray, (end - inarray) * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(device_result, inarray, (end - inarray) * sizeof(int), cudaMemcpyHostToDevice);
+    // 为了方便调试，申请一块调试用内存
+    cudaMemcpy(device_debug, inarray, (end - inarray) * sizeof(int), cudaMemcpyHostToDevice);
 
     double startTime = CycleTimer::currentSeconds();
 
-    exclusive_scan(device_input, N, device_result);
+    // 为了方便调试，申请一块调试用内存
+    // exclusive_scan(device_input, N, device_result);
+    exclusive_scan(device_input, N, device_result, device_debug);
 
     // Wait for completion
     cudaDeviceSynchronize();
